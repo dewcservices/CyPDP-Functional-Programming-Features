49c9122f1056be8565b086ecb3a11533b5a6249f
USER5 <us3r5@hotmail.com>
2024-07-26 18:25:27 +0930

    feat: Simplify pure functions

--- examples/07-pure-functions.ts
+++ examples/07-pure-functions.ts
@@ -17,32 +17,33 @@ for (let x = 0; x < 10; x++) {
 
 // Alternatively:
 
+type State = {
+  num: number;
+  increase: number;
+};
+
 function getNextNum2(
-  current: number,
-
-  // don't use globals - only depend on arguments
-  increase: number,
-
+  currentState: State,
   // Let the user provide the "increase" logic
   updateIncreaseFn: () => number,
-
   // We return all the information that needs to be updated
-): { nextNum: number; nextIncrease: number } {
-  const nextIncrease = increase + updateIncreaseFn();
+): State {
+  const nextIncrease = currentState.increase + updateIncreaseFn();
   return {
-    nextIncrease,
-    nextNum: current + nextIncrease,
+    increase: nextIncrease,
+    num: currentState.num + nextIncrease,
   };
 }
 
-let currentNum2 = 10;
 // We don't HAVE to use random... But we CAN if we want
 let increaseFn = () => Math.round(Math.random() * 10);
-let increase2 = 5;
+
+let state: State = {
+  increase: 5,
+  num: 10,
+};
 
 for (let x = 0; x < 10; x++) {
-  const update = getNextNum2(currentNum2, increase2, increaseFn);
-  currentNum2 = update.nextNum;
-  increase2 = update.nextIncrease;
-  console.log(currentNum2);
+  state = getNextNum2(state, increaseFn);
+  console.log(state.num);
 }
